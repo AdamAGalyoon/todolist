64b3980c94164f25c42bc874b1e22b74aadcc757
AdamAGalyoon <144166903+AdamAGalyoon@users.noreply.github.com>
2024-03-06 21:57:16 -0800

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ $username = 'root';
 $password = ''; 
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$todolist", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Error: Could not connect. " . $e->getMessage());
